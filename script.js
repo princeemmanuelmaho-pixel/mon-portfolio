8e637d8839864c9b3d12e83438ec2ee26298e88e
princeemmanuelmaho-pixel <prince.emmanuel.maho@gmail.com>
2026-02-09 15:48:45 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function afficherMessage() {
+    var leMessage = document.getElementById('messageSecret');
+    var leBouton = document.getElementById('welcomeBtn');
+    leMessage.style.display = 'block';
+    leBouton.style.display = 'none';
+}
+
